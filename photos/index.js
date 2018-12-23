02337375386632941889d1ab9d52b3f0d1854d6a
Eero Vilpponen <eero.vilpponen@gmail.com>
2018-12-23 17:09:11 +0200

    Initial commit

---
+++ photos/index.js
@@ -0,0 +1,167 @@
+console.log("Hello world!");
+
+var GoogleAuth;
+var SCOPE = "https://www.googleapis.com/auth/photoslibrary.readonly";
+function handleClientLoad() {
+  // Load the API's client and auth2 modules.
+  // Call the initClient function after the modules load.
+  gapi.load("client:auth2", initClient);
+}
+
+function initClient() {
+  // Retrieve the discovery document for version 3 of Google Drive API.
+  // In practice, your app can retrieve one or more discovery documents.
+  var discoveryUrl =
+    "https://photoslibrary.googleapis.com/$discovery/rest?version=v1";
+
+  // Initialize the gapi.client object, which app uses to make API requests.
+  // Get API key and client ID from API Console.
+  // 'scope' field specifies space-delimited list of access scopes.
+  gapi.client
+    .init({
+      apiKey: "AIzaSyBLgTeDjqm0X3800xEQzxo9bCa3HYIfpns",
+      discoveryDocs: [discoveryUrl],
+      clientId: "734651298434-6lukaeulk76bjmj4pkv456lt1bbd1f8v.apps.googleusercontent.com",
+      scope: SCOPE
+    })
+    .then(function() {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+      // Handle initial sign-in state. (Determine if user is already signed in.)
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
+
+      // Call handleAuthClick function when user clicks on
+      //      "Sign In/Authorize" button.
+      $("#sign-in-or-out-button").click(function() {
+        handleAuthClick();
+      });
+      $("#revoke-access-button").click(function() {
+        revokeAccess();
+      });
+    });
+}
+
+function handleAuthClick() {
+  if (GoogleAuth.isSignedIn.get()) {
+    // User is authorized and has clicked 'Sign out' button.
+    GoogleAuth.signOut();
+  } else {
+    // User is not signed in. Start Google auth flow.
+    GoogleAuth.signIn();
+  }
+}
+
+function revokeAccess() {
+  GoogleAuth.disconnect();
+}
+
+function setSigninStatus(isSignedIn) {
+  var user = GoogleAuth.currentUser.get();
+  var isAuthorized = user.hasGrantedScopes(SCOPE);
+  if (isAuthorized) {
+    $("#sign-in-or-out-button").css("display", "none")//.html("Sign out");
+    $("#revoke-access-button").css("display", "none");//"inline-block");
+    $("#auth-status").html('');
+    //   "You are currently signed in and have granted " + "access to this app."
+    // );
+    listPhotos();
+  } else {
+    $("#sign-in-or-out-button").html("Sign In/Authorize");
+    $("#revoke-access-button").css("display", "none");
+    $("#auth-status").html(
+      "You have not authorized this app or you are " + "signed out."
+    );
+  }
+}
+
+function updateSigninStatus(isSignedIn) {
+  setSigninStatus();
+}
+
+function setPhoto(mediaItem) {
+    const photo_big = document.getElementById('photo_big');
+    let el;
+    if (mediaItem.mimeType.indexOf('image/') === 0) {
+        const url = buildFullUrl(mediaItem);
+        el = document.createElement('img');
+        el.src = url;
+    } else if (mediaItem.mimeType.indexOf('video/') === 0) {
+        const url = mediaItem.baseUrl + "=dv";
+        el = document.createElement('video');
+        el.autoplay = true;
+        el.height = screen.height;
+        el.width = screen.width * 0.8;
+        el.src = url;
+        el.addEventListener('loadstart', function (event) {
+            photo_big.classList.add('loading');
+            photo_big.poster = '';
+        });
+        el.addEventListener('canplay', function (event) {
+            photo_big.classList.remove('loading');
+            photo_big.poster = '';
+        });
+    } else {
+        return;
+    }
+    if (photo_big.firstChild) {
+        photo_big.replaceChild(el, photo_big.firstChild);
+    } else {
+        photo_big.appendChild(el);
+    }
+    // console.log(img.offsetWidth);
+    // img.style['margin-left'] = `-${img.offsetWidth / 2}px`;
+}
+
+function buildFullUrl(mediaItem) {
+    return mediaItem.baseUrl + "=w2048-h1024";
+}
+function thumbnail(mediaItem) {
+    return mediaItem.baseUrl;
+}
+var nextPageToken = null; 
+var listing = true;
+function listPhotos() {
+    listing = true;
+    // Example 2: Use gapi.client.request(args) function
+    var request = gapi.client.request({
+    'method': 'GET',
+    'path': 'https://photoslibrary.googleapis.com/v1/mediaItems',
+    'params': {pageToken: nextPageToken}
+    });
+    // Execute the API request.
+    request.execute(function(response) {
+        mediaItems = response.mediaItems;
+        const photos_list = document.getElementById('photos_list');
+        if (nextPageToken === null) {
+            setPhoto(mediaItems[0]);
+        }
+        nextPageToken = response.nextPageToken;
+        for (let i=0; i<mediaItems.length; i++) {
+            let el = document.createElement('a')
+            el.href = "#"
+            el.onclick = function(event) {
+                event.preventDefault();
+                setPhoto(mediaItems[i]);
+                return false;
+            }
+            let img = document.createElement('IMG');
+            img.src = thumbnail(mediaItems[i]);
+            el.appendChild(img);
+            photos_list.appendChild(el);
+        }
+        listing = false;
+        console.log(response);
+    });   
+}
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 5000) {
+        if (!listing){
+            listPhotos();
+        }
+    }
+};
